56f1b1e207829f8809e7b0f4103aeab3fc270954
barakman <barakman@yahoo.com>
2019-11-21 12:38:28 +0200

    Cosmetic.

--- solidity/test/FixedSupplyUpgrader.js
+++ solidity/test/FixedSupplyUpgrader.js
@@ -6,7 +6,7 @@ contract("FixedSupplyUpgrader", function(accounts) {
     let oldConverter;
     let newConverter;
     let oldUpgrader;
-    let fixedSupplyUpgrader;
+    let newUpgrader;
 
     const ETH_TOKEN_AMOUNT = 111;
     const BNT_TOKEN_AMOUNT = 222;
@@ -27,23 +27,23 @@ contract("FixedSupplyUpgrader", function(accounts) {
         oldConverter        = await artifacts.require("BancorConverter"    ).new(bntToken.address  , registry.address, 0, ethToken.address, 100000, {from: deployer});
         newConverter        = await artifacts.require("BancorConverter"    ).new(relayToken.address, registry.address, 0, ethToken.address, 500000, {from: deployer});
         oldUpgrader         = await artifacts.require("BancorConverterUpgrader").new(registry.address, {from: upgrader});
-        fixedSupplyUpgrader = await artifacts.require("FixedSupplyUpgrader").new({from: upgrader});
-        await registry.registerAddress(web3.fromAscii("BancorConverterUpgrader"), fixedSupplyUpgrader.address, {from: deployer});
+        newUpgrader = await artifacts.require("FixedSupplyUpgrader").new({from: upgrader});
+        await registry.registerAddress(web3.fromAscii("BancorConverterUpgrader"), newUpgrader.address, {from: deployer});
         await newConverter.addConnector(bntToken.address, 500000, false, {from: deployer});
-        await relayToken  .transferOwnership(fixedSupplyUpgrader.address, {from: deployer});
-        await oldConverter.transferOwnership(fixedSupplyUpgrader.address, {from: deployer});
-        await newConverter.transferOwnership(fixedSupplyUpgrader.address, {from: deployer});
+        await relayToken  .transferOwnership(newUpgrader.address, {from: deployer});
+        await oldConverter.transferOwnership(newUpgrader.address, {from: deployer});
+        await newConverter.transferOwnership(newUpgrader.address, {from: deployer});
         await ethToken.deposit({from: deployer, value: ETH_TOKEN_AMOUNT});
         await ethToken.transfer(oldConverter.address, ETH_TOKEN_AMOUNT, {from: deployer});
         await bntToken.issue(upgrader, BNT_TOKEN_AMOUNT + BNT_TOKEN_BUFFER + BNT_TOKEN_REMAIN, {from: deployer});
-        await bntToken.transfer(fixedSupplyUpgrader.address, BNT_TOKEN_AMOUNT + BNT_TOKEN_BUFFER, {from: upgrader});
+        await bntToken.transfer(newUpgrader.address, BNT_TOKEN_AMOUNT + BNT_TOKEN_BUFFER, {from: upgrader});
         await bntToken.transferOwnership(oldConverter.address, {from: deployer});
         await oldConverter.acceptTokenOwnership({from: deployer});
     });
 
     it("function execute should abort with an error if called by a non-owner", async function() {
-        await catchRevert(fixedSupplyUpgrader.execute(oldConverter.address, newConverter.address, airDropper, BNT_TOKEN_AMOUNT, {from: deployer}));
-        await assertBalance(bntToken  , fixedSupplyUpgrader.address, BNT_TOKEN_AMOUNT + BNT_TOKEN_BUFFER);
+        await catchRevert(newUpgrader.execute(oldConverter.address, newConverter.address, airDropper, BNT_TOKEN_AMOUNT, {from: deployer}));
+        await assertBalance(bntToken  , newUpgrader.address, BNT_TOKEN_AMOUNT + BNT_TOKEN_BUFFER);
         await assertBalance(bntToken  , newConverter       .address, 0);
         await assertBalance(bntToken  , upgrader                   , BNT_TOKEN_REMAIN);
         await assertBalance(ethToken  , oldConverter       .address, ETH_TOKEN_AMOUNT);
@@ -56,8 +56,8 @@ contract("FixedSupplyUpgrader", function(accounts) {
     });
 
     it("function execute should complete successfully if called by the owner", async function() {
-        await fixedSupplyUpgrader.execute(oldConverter.address, newConverter.address, airDropper, BNT_TOKEN_AMOUNT, {from: upgrader});
-        await assertBalance(bntToken  , fixedSupplyUpgrader.address, 0);
+        await newUpgrader.execute(oldConverter.address, newConverter.address, airDropper, BNT_TOKEN_AMOUNT, {from: upgrader});
+        await assertBalance(bntToken  , newUpgrader.address, 0);
         await assertBalance(bntToken  , newConverter       .address, BNT_TOKEN_AMOUNT);
         await assertBalance(bntToken  , upgrader                   , BNT_TOKEN_REMAIN + BNT_TOKEN_BUFFER);
         await assertBalance(ethToken  , oldConverter       .address, 0);
@@ -65,15 +65,15 @@ contract("FixedSupplyUpgrader", function(accounts) {
         await assertBalance(relayToken, airDropper                 , BNT_TOKEN_AMOUNT);
         await assertBalance(relayToken, upgrader                   , BNT_TOKEN_AMOUNT);
         await assertOwner(relayToken  , newConverter       .address);
-        await assertOwner(oldConverter, fixedSupplyUpgrader.address);
-        await assertOwner(newConverter, fixedSupplyUpgrader.address);
+        await assertOwner(oldConverter, newUpgrader.address);
+        await assertOwner(newConverter, newUpgrader.address);
     });
 
     it("accepting ownership should abort with an error if called by a non-owner", async function() {
         await catchRevert(oldConverter.acceptOwnership({from: deployer}));
         await catchRevert(newConverter.acceptOwnership({from: deployer}));
-        await assertOwner(oldConverter, fixedSupplyUpgrader.address);
-        await assertOwner(newConverter, fixedSupplyUpgrader.address);
+        await assertOwner(oldConverter, newUpgrader.address);
+        await assertOwner(newConverter, newUpgrader.address);
     });
 
     it("accepting ownership should complete successfully if called by the owner", async function() {
@@ -85,7 +85,7 @@ contract("FixedSupplyUpgrader", function(accounts) {
 
     it("reinstating upgrader should abort with an error if called by a non-owner", async function() {
         await catchRevert(registry.registerAddress(web3.fromAscii("BancorConverterUpgrader"), oldUpgrader.address, {from: upgrader}));
-        assert.equal(await registry.addressOf(web3.fromAscii("BancorConverterUpgrader")), fixedSupplyUpgrader.address);
+        assert.equal(await registry.addressOf(web3.fromAscii("BancorConverterUpgrader")), newUpgrader.address);
     });
 
     it("reinstating upgrader should complete successfully if called by the owner", async function() {
