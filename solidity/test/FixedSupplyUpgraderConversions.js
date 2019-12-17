09e716e63562b417ef6c2696a471694a4b8753c8
barakman <barakman@yahoo.com>
2019-12-17 17:06:22 +0200

    Upgrade `bancor-contracts` version from 0.5.10 to 0.5.11.

--- solidity/test/FixedSupplyUpgraderConversions.js
+++ solidity/test/FixedSupplyUpgraderConversions.js
@@ -89,58 +89,60 @@ contract("FixedSupplyUpgraderConversions", function(accounts) {
         }
     };
 
-    let contractRegistry        ;
-    let contractFeatures        ;
-    let fixedSupplyUpgrader     ;
-    let nonStandardTokenRegistry;
-    let bancorFormula           ;
-    let bancorNetwork           ;
-    let bancorNetworkPathFinder ;
-    let bancorConverterUpgrader ;
-    let bancorConverterRegistry ;
-    let bancorGasPriceLimit     ;
-    let etherToken              ;
-    let smartToken1             ;
-    let smartToken2             ;
-    let smartToken3             ;
-    let smartToken4             ;
-    let smartToken5             ;
-    let erc20TokenA             ;
-    let erc20TokenB             ;
-    let bancorConverter1        ;
-    let bancorConverter2        ;
-    let bancorConverter3        ;
-    let bancorConverter4        ;
-    let bancorConverter5        ;
-    let gasPrice                ;
+    let contractRegistry           ;
+    let contractFeatures           ;
+    let fixedSupplyUpgrader        ;
+    let nonStandardTokenRegistry   ;
+    let bancorFormula              ;
+    let bancorNetwork              ;
+    let bancorNetworkPathFinder    ;
+    let bancorConverterUpgrader    ;
+    let bancorConverterRegistry    ;
+    let bancorConverterRegistryData;
+    let bancorGasPriceLimit        ;
+    let etherToken                 ;
+    let smartToken1                ;
+    let smartToken2                ;
+    let smartToken3                ;
+    let smartToken4                ;
+    let smartToken5                ;
+    let erc20TokenA                ;
+    let erc20TokenB                ;
+    let bancorConverter1           ;
+    let bancorConverter2           ;
+    let bancorConverter3           ;
+    let bancorConverter4           ;
+    let bancorConverter5           ;
+    let gasPrice                   ;
 
     const paths = {};
 
     before(async function() {
-        contractRegistry         = await artifacts.require("ContractRegistry"        ).new();
-        contractFeatures         = await artifacts.require("ContractFeatures"        ).new();
-        fixedSupplyUpgrader      = await artifacts.require("FixedSupplyUpgrader"     ).new();
-        nonStandardTokenRegistry = await artifacts.require("NonStandardTokenRegistry").new();
-        bancorFormula            = await artifacts.require("BancorFormula"           ).new();
-        bancorNetwork            = await artifacts.require("BancorNetwork"           ).new(contractRegistry.address);
-        bancorNetworkPathFinder  = await artifacts.require("BancorNetworkPathFinder" ).new(contractRegistry.address);
-        bancorConverterUpgrader  = await artifacts.require("BancorConverterUpgrader" ).new(contractRegistry.address);
-        bancorConverterRegistry  = await artifacts.require("BancorConverterRegistry" ).new();
-        bancorGasPriceLimit      = await artifacts.require("BancorGasPriceLimit"     ).new(config.priceLimitParams.value);
-        etherToken               = await artifacts.require("EtherToken"              ).new();
-        smartToken1              = await artifacts.require("SmartToken"              ).new(config.smartToken1Params.name, config.smartToken1Params.symbol, config.smartToken1Params.decimals);
-        smartToken2              = await artifacts.require("SmartToken"              ).new(config.smartToken2Params.name, config.smartToken2Params.symbol, config.smartToken2Params.decimals);
-        smartToken3              = await artifacts.require("SmartToken"              ).new(config.smartToken3Params.name, config.smartToken3Params.symbol, config.smartToken3Params.decimals);
-        smartToken4              = await artifacts.require("SmartToken"              ).new(config.smartToken4Params.name, config.smartToken4Params.symbol, config.smartToken4Params.decimals);
-        smartToken5              = await artifacts.require("SmartToken"              ).new(config.smartToken5Params.name, config.smartToken5Params.symbol, config.smartToken5Params.decimals);
-        erc20TokenA              = await artifacts.require("ERC20Token"              ).new(config.erc20TokenAParams.name, config.erc20TokenAParams.symbol, config.erc20TokenAParams.decimals, config.erc20TokenAParams.supply);
-        erc20TokenB              = await artifacts.require("ERC20Token"              ).new(config.erc20TokenBParams.name, config.erc20TokenBParams.symbol, config.erc20TokenBParams.decimals, config.erc20TokenBParams.supply);
-        bancorConverter1         = await artifacts.require("BancorConverter"         ).new(smartToken1.address, contractRegistry.address, config.converter1Params.fee, etherToken .address, config.converter1Params.ratio1);
-        bancorConverter2         = await artifacts.require("BancorConverter"         ).new(smartToken2.address, contractRegistry.address, config.converter2Params.fee, smartToken1.address, config.converter2Params.ratio1);
-        bancorConverter3         = await artifacts.require("BancorConverter"         ).new(smartToken3.address, contractRegistry.address, config.converter3Params.fee, smartToken1.address, config.converter3Params.ratio1);
-        bancorConverter4         = await artifacts.require("BancorConverter"         ).new(smartToken4.address, contractRegistry.address, config.converter4Params.fee, smartToken1.address, config.converter4Params.ratio1);
-        bancorConverter5         = await artifacts.require("BancorConverter"         ).new(smartToken5.address, contractRegistry.address, config.converter5Params.fee, smartToken1.address, config.converter5Params.ratio1);
-        gasPrice                 = await bancorGasPriceLimit.gasPrice();
+        contractRegistry            = await artifacts.require("ContractRegistry"           ).new();
+        contractFeatures            = await artifacts.require("ContractFeatures"           ).new();
+        fixedSupplyUpgrader         = await artifacts.require("FixedSupplyUpgrader"        ).new();
+        nonStandardTokenRegistry    = await artifacts.require("NonStandardTokenRegistry"   ).new();
+        bancorFormula               = await artifacts.require("BancorFormula"              ).new();
+        bancorNetwork               = await artifacts.require("BancorNetwork"              ).new(contractRegistry.address);
+        bancorNetworkPathFinder     = await artifacts.require("BancorNetworkPathFinder"    ).new(contractRegistry.address);
+        bancorConverterUpgrader     = await artifacts.require("BancorConverterUpgrader"    ).new(contractRegistry.address);
+        bancorConverterRegistry     = await artifacts.require("BancorConverterRegistry"    ).new(contractRegistry.address);
+        bancorConverterRegistryData = await artifacts.require("BancorConverterRegistryData").new(contractRegistry.address);
+        bancorGasPriceLimit         = await artifacts.require("BancorGasPriceLimit"        ).new(config.priceLimitParams.value);
+        etherToken                  = await artifacts.require("EtherToken"                 ).new();
+        smartToken1                 = await artifacts.require("SmartToken"                 ).new(config.smartToken1Params.name, config.smartToken1Params.symbol, config.smartToken1Params.decimals);
+        smartToken2                 = await artifacts.require("SmartToken"                 ).new(config.smartToken2Params.name, config.smartToken2Params.symbol, config.smartToken2Params.decimals);
+        smartToken3                 = await artifacts.require("SmartToken"                 ).new(config.smartToken3Params.name, config.smartToken3Params.symbol, config.smartToken3Params.decimals);
+        smartToken4                 = await artifacts.require("SmartToken"                 ).new(config.smartToken4Params.name, config.smartToken4Params.symbol, config.smartToken4Params.decimals);
+        smartToken5                 = await artifacts.require("SmartToken"                 ).new(config.smartToken5Params.name, config.smartToken5Params.symbol, config.smartToken5Params.decimals);
+        erc20TokenA                 = await artifacts.require("ERC20Token"                 ).new(config.erc20TokenAParams.name, config.erc20TokenAParams.symbol, config.erc20TokenAParams.decimals, config.erc20TokenAParams.supply);
+        erc20TokenB                 = await artifacts.require("ERC20Token"                 ).new(config.erc20TokenBParams.name, config.erc20TokenBParams.symbol, config.erc20TokenBParams.decimals, config.erc20TokenBParams.supply);
+        bancorConverter1            = await artifacts.require("BancorConverter"            ).new(smartToken1.address, contractRegistry.address, config.converter1Params.fee, etherToken .address, config.converter1Params.ratio1);
+        bancorConverter2            = await artifacts.require("BancorConverter"            ).new(smartToken2.address, contractRegistry.address, config.converter2Params.fee, smartToken1.address, config.converter2Params.ratio1);
+        bancorConverter3            = await artifacts.require("BancorConverter"            ).new(smartToken3.address, contractRegistry.address, config.converter3Params.fee, smartToken1.address, config.converter3Params.ratio1);
+        bancorConverter4            = await artifacts.require("BancorConverter"            ).new(smartToken4.address, contractRegistry.address, config.converter4Params.fee, smartToken1.address, config.converter4Params.ratio1);
+        bancorConverter5            = await artifacts.require("BancorConverter"            ).new(smartToken5.address, contractRegistry.address, config.converter5Params.fee, smartToken1.address, config.converter5Params.ratio1);
+        gasPrice                    = await bancorGasPriceLimit.gasPrice();
 
         await etherToken.deposit({value: config.etherTokenParams.supply});
         await smartToken1.issue(account, config.smartToken1Params.supply);
@@ -150,22 +152,18 @@ contract("FixedSupplyUpgraderConversions", function(accounts) {
         await bancorConverter3.addReserve(erc20TokenA.address, config.converter3Params.ratio2);
         await bancorConverter4.addReserve(erc20TokenB.address, config.converter4Params.ratio2);
         await bancorConverter5.addReserve(etherToken .address, config.converter5Params.ratio2);
-        await contractRegistry.registerAddress(web3.fromAscii("ContractRegistry"        ), contractRegistry        .address);
-        await contractRegistry.registerAddress(web3.fromAscii("ContractFeatures"        ), contractFeatures        .address);
-        await contractRegistry.registerAddress(web3.fromAscii("NonStandardTokenRegistry"), nonStandardTokenRegistry.address);
-        await contractRegistry.registerAddress(web3.fromAscii("BancorFormula"           ), bancorFormula           .address);
-        await contractRegistry.registerAddress(web3.fromAscii("BancorNetwork"           ), bancorNetwork           .address);
-        await contractRegistry.registerAddress(web3.fromAscii("BancorNetworkPathFinder" ), bancorNetworkPathFinder .address);
-        await contractRegistry.registerAddress(web3.fromAscii("BancorConverterUpgrader" ), bancorConverterUpgrader .address);
-        await contractRegistry.registerAddress(web3.fromAscii("BancorConverterRegistry" ), bancorConverterRegistry .address);
-        await contractRegistry.registerAddress(web3.fromAscii("BancorGasPriceLimit"     ), bancorGasPriceLimit     .address);
-        await contractRegistry.registerAddress(web3.fromAscii("EtherToken"              ), etherToken              .address);
-        await contractRegistry.registerAddress(web3.fromAscii("BNTToken"                ), smartToken1             .address);
-        await contractRegistry.registerAddress(web3.fromAscii("BNTConverter"            ), bancorConverter1        .address);
-        await bancorConverterRegistry.registerConverter(smartToken1.address, bancorConverter1.address);
-        await bancorConverterRegistry.registerConverter(smartToken2.address, bancorConverter2.address);
-        await bancorConverterRegistry.registerConverter(erc20TokenA.address, bancorConverter3.address);
-        await bancorConverterRegistry.registerConverter(erc20TokenB.address, bancorConverter4.address);
+        await contractRegistry.registerAddress(web3.fromAscii("ContractRegistry"           ), contractRegistry           .address);
+        await contractRegistry.registerAddress(web3.fromAscii("ContractFeatures"           ), contractFeatures           .address);
+        await contractRegistry.registerAddress(web3.fromAscii("NonStandardTokenRegistry"   ), nonStandardTokenRegistry   .address);
+        await contractRegistry.registerAddress(web3.fromAscii("BancorFormula"              ), bancorFormula              .address);
+        await contractRegistry.registerAddress(web3.fromAscii("BancorNetwork"              ), bancorNetwork              .address);
+        await contractRegistry.registerAddress(web3.fromAscii("BancorNetworkPathFinder"    ), bancorNetworkPathFinder    .address);
+        await contractRegistry.registerAddress(web3.fromAscii("BancorConverterUpgrader"    ), bancorConverterUpgrader    .address);
+        await contractRegistry.registerAddress(web3.fromAscii("BancorConverterRegistry"    ), bancorConverterRegistry    .address);
+        await contractRegistry.registerAddress(web3.fromAscii("BancorConverterRegistryData"), bancorConverterRegistryData.address);
+        await contractRegistry.registerAddress(web3.fromAscii("BancorGasPriceLimit"        ), bancorGasPriceLimit        .address);
+        await contractRegistry.registerAddress(web3.fromAscii("BNTToken"                   ), smartToken1                .address);
+        await contractRegistry.registerAddress(web3.fromAscii("BNTConverter"               ), bancorConverter1           .address);
         await etherToken .transfer(bancorConverter1.address, config.converter1Params.reserve1);
         await smartToken1.transfer(bancorConverter2.address, config.converter2Params.reserve1);
         await smartToken1.transfer(bancorConverter3.address, config.converter3Params.reserve1);
@@ -180,7 +178,11 @@ contract("FixedSupplyUpgraderConversions", function(accounts) {
         await bancorConverter2.acceptTokenOwnership();
         await bancorConverter3.acceptTokenOwnership();
         await bancorConverter4.acceptTokenOwnership();
-        await bancorNetworkPathFinder.updateAnchorToken();
+        await bancorConverterRegistry.addConverter(bancorConverter1.address);
+        await bancorConverterRegistry.addConverter(bancorConverter2.address);
+        await bancorConverterRegistry.addConverter(bancorConverter3.address);
+        await bancorConverterRegistry.addConverter(bancorConverter4.address);
+        await bancorNetworkPathFinder.setAnchorToken(etherToken.address));
         await bancorNetwork.registerEtherToken(etherToken.address, true);
 
         for (const token of [smartToken1, smartToken2, erc20TokenA, erc20TokenB, etherToken, smartToken3, smartToken4, smartToken5]) {
@@ -196,7 +198,7 @@ contract("FixedSupplyUpgraderConversions", function(accounts) {
         for (const sourceToken of [smartToken1, smartToken2, erc20TokenA, erc20TokenB]) {
             paths[sourceToken.address] = {};
             for (const targetToken of [smartToken1, smartToken2, erc20TokenA, erc20TokenB]) {
-                paths[sourceToken.address][targetToken.address] = await bancorNetworkPathFinder.get(sourceToken.address, targetToken.address, [bancorConverterRegistry.address]);
+                paths[sourceToken.address][targetToken.address] = await bancorNetworkPathFinder.generatePath(sourceToken.address, targetToken.address);
             }
         }
         paths[etherToken.address] = {};
